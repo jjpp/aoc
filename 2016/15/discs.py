bc07b12fdd2a7e762bfa4749a0a105363e4c9b44
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2023-12-06 23:02:20 +0100

    2016: 15

---
+++ 2016/15/discs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+from math import prod
+
+ls = [l.strip() for l in sys.stdin]
+
+n = []
+a = []
+
+for l in ls:
+    d = l.split()
+    n.append(int(d[3]))
+    a.append(int(d[11][:-1]))
+
+def check(x):
+    for i in range(0, len(n)):
+        if (x + 1 + i + a[i]) % n[i] != 0:
+            return False
+    return True
+
+for x in range(0, prod(n)):
+    if check(x):
+        print(x)
+        break
+
+
