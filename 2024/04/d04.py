7679cdb8c43d5419ea3bfd0575b87e342e649b48
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2024-12-04 06:27:04 +0100

    2024: 04

---
+++ 2024/04/d04.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys
+
+ls = [l.strip() for l in sys.stdin]
+
+X = len(ls[0])
+Y = len(ls)
+
+c = 0
+
+for j in range(0, Y):
+    for i in range(0, X):
+        if i <= X - 4:
+            test = ls[j][i:i+4]
+            if test == 'XMAS':
+                c += 1
+            if test == 'SAMX':
+                c += 1
+        if j <= Y - 4:
+            test = "".join([ls[j + j_][i] for j_ in range(0, 4)])
+            if test == 'XMAS':
+                c += 1
+            if test == 'SAMX':
+                c += 1
+        if i <= X - 4 and j >= 3:
+            test = "".join([ls[j - j_][i + j_] for j_ in range(0, 4)])
+            if test == 'XMAS':
+                c += 1
+            if test == 'SAMX':
+                c += 1
+
+        if i <= X - 4 and j <= Y - 4:
+            test = "".join([ls[j + j_][i + j_] for j_ in range(0, 4)])
+            if test == 'XMAS':
+                c += 1
+            if test == 'SAMX':
+                c += 1
+
+print(c)
+
---
+++ 2024/04/d04_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+ls = [l.strip() for l in sys.stdin]
+
+X = len(ls[0])
+Y = len(ls)
+
+c = 0
+
+for j in range(0, Y - 2):
+    for i in range(0, X - 2):
+        t1 = "".join([ls[j + j_][i+j_] for j_ in range(0, 3)])
+        t2 = "".join([ls[j + 2 - j_][i+j_] for j_ in range(0, 3)])
+
+        if (t1 in ['MAS', 'SAM']) and (t2 in ['MAS', 'SAM']):
+            c += 1
+
+print(c)
+
