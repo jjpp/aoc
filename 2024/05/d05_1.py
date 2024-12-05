d3b830278addf31e60c032bf9c754498b12d9a56
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2024-12-05 06:41:38 +0100

    2024: 05, both together

---
+++ 2024/05/d05_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys
+
+ls = [l.strip() for l in sys.stdin]
+
+rs = []
+before = {}
+
+while len(ls) > 0 and '|' in ls[0]:
+    (x, y) = ls[0].split('|')
+    rs.append((x, y))
+    if x not in before:
+        before[x] = set()
+    before[x] |= {y}
+    ls.pop(0)
+
+if ls[0] == '':
+    ls.pop(0)
+
+
+class page(str):
+    def __lt__(self, b):
+        if self not in before:
+            return False
+        return b in before[self]
+
+s1 = 0
+s2 = 0
+for l in ls:
+    lls = l.split(',')
+    lls_ = sorted([page(x) for x in lls])
+    mid = int(lls_[int(len(lls) / 2)])
+    if lls == lls_:
+        s1 += mid
+    else:
+        s2 += mid
+
+print(s1)
+print(s2)
+
