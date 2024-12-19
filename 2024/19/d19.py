4f951a9cadbab2c5d4ca064e5f1952a06d4c0060
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2024-12-19 06:23:16 +0100

    2024: 19

---
+++ 2024/19/d19.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+ls = [l.strip() for l in sys.stdin]
+
+towels = list(sorted(ls[0].split(', ')))
+
+p = dict()
+
+def possible(l):
+    if l in towels:
+        return 1
+    if l in p:
+        return p[l]
+    for t in towels:
+        if l[0:len(t)] == t and possible(l[len(t):]) == 1:
+            p[l] = 1
+            return 1
+    p[l] = 0
+    return 0
+
+print(sum([possible(l) for l in ls[2:]]))
+
+
---
+++ 2024/19/d19_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+ls = [l.strip() for l in sys.stdin]
+
+towels = list(sorted(ls[0].split(', ')))
+
+p = dict()
+
+p[""] = 0
+
+def possible(l):
+    if l in p:
+        return p[l]
+
+    p[l] = 1 if l in towels else 0
+
+    for t in towels:
+        if l[0:len(t)] == t:
+            p[l] += possible(l[len(t):])
+
+    return p[l]
+
+print(sum([possible(l) for l in ls[2:]]))
+
+
